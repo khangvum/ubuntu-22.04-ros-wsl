810e17e578e1aaab60c6b94253fb857e355e637e
khangvum <manhkhang0305@gmail.com>
2025-03-22 20:59:53 -0400

    Add wsl.sh

---
+++ wsl.sh
@@ -0,0 +1,21 @@
+# Configure WSL global settings
+sudo tee /etc/wsl.conf > /dev/null << 'EOF'
+[boot]
+systemd=true
+
+[automount]
+enabled=true
+root=/mnt/
+
+[interop]
+appendWindowsPath=true
+enabled=true
+
+[network]
+generateHosts=true
+generateResolvConf=true
+hostname=ubuntu
+
+[user]
+default=khangvum
+EOF
