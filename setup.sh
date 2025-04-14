3595d285cdde5b5acd2e53d0c1a24d3386573a98
khangvum <manhkhang0305@gmail.com>
2025-04-14 19:55:03 -0400

    Updated cleanup path

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Define the setup directory path
-DOTFILES_PATH="/etc/ubuntu/.dotfiles/setup"
+SETUP_PATH="/etc/ubuntu/.dotfiles/setup"
 
 # Install the required updates
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 
 # Run the setup scripts
 # - Add aliases
-bash $DOTFILES_PATH/aliases.sh
+bash $SETUP_PATH/aliases.sh
 # - Install the packages and dependencies
-bash $DOTFILES_PATH/packages.sh
+bash $SETUP_PATH/packages.sh
 # - Install ROS 2
-bash $DOTFILES_PATH/ros.sh
+bash $SETUP_PATH/ros.sh
 # - Configure WSL settings
-bash $DOTFILES_PATH/wsl.sh
+bash $SETUP_PATH/wsl.sh
 
 # Power off the distro
 sudo shutdown -h now
