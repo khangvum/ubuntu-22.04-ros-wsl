10e61882fb2de6970e8cf649e8e5d983b086dcd8
khangvum <manhkhang0305@gmail.com>
2025-04-18 16:16:03 -0400

    Added git and user folders removal

--- cleanup.sh
+++ cleanup.sh
@@ -11,5 +11,18 @@ bash $CLEANUP_PATH/ros.sh
 # - Remove default user profile from WSL settings
 bash $CLEANUP_PATH/wsl.sh
 
+# Remove git folders
+sudo rm -R /etc/ubuntu/.dotfiles/.git
+rm /etc/ubuntu/.dotfiles/LICENSE
+rm /etc/ubuntu/.dotfiles/README.md
+
+# Remove any user profile in the /home directory
+for dir in /home/*; do
+    if [ -d "$dir" ]; then
+        echo -e "\e[33mRemoving /home/$package\e[0m"
+        rm -rf "$dir"
+    fi
+done
+
 # Clean up the system
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
